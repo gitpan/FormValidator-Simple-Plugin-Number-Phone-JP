0b1faaa0e0eb863133273efce3d61648a3f9ec4b
Gosuke Miyashita <gosukenator@gmail.com>
2008-03-09 03:50:03 +0000

    Import of MIZZY/FormValidator-Simple-Plugin-Number-Phone-JP-0.03 from CPAN.
    gitpan-cpan-distribution: FormValidator-Simple-Plugin-Number-Phone-JP
    gitpan-cpan-version:      0.03
    gitpan-cpan-path:         MIZZY/FormValidator-Simple-Plugin-Number-Phone-JP-0.03.tar.gz
    gitpan-cpan-author:       MIZZY
    gitpan-cpan-maturity:     released

--- lib/FormValidator/Simple/Plugin/Number/Phone/JP.pm
+++ lib/FormValidator/Simple/Plugin/Number/Phone/JP.pm
@@ -4,7 +4,7 @@ use strict;
 use Number::Phone::JP;
 use FormValidator::Simple::Constants;
 
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 our @CARP_NOT = qw(Number::Phone::JP);
 
 sub NUMBER_PHONE_JP {
